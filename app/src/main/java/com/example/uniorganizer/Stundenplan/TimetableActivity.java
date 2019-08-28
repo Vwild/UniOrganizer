167bce650e21f38f11487e7f1fcc13be08f275df
Valentin Wild <Valentin.Wild@stud.uni-regensburg.de>
2019-08-28 13:17:19 +0200

    First Commit, Valentin

---
+++ app/src/main/java/com/example/uniorganizer/Stundenplan/TimetableActivity.java
@@ -0,0 +1,14 @@
+package com.example.uniorganizer.Stundenplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.uniorganizer.R;
+
+public class TimetableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable);
+    }
+}
