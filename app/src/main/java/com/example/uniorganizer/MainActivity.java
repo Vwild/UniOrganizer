ae78e69da7fc8d57dd891af00df548542503c381
SirSpliffington <vincent.eichenseher@stud.uni-regensburg.de>
2019-09-01 19:48:24 +0200

    Erstellen der Datenbank für den Stundenplan und die dazugehörige Entity und Dao. In dem Dao stehen einige methoden bereit um auf Elemente in der Datenbank zuzugreifen und weiter zu verwenden. Des weiteren wurde die Datenbank als Instanzvariable in der MainActivity Festgelegt und eine methode bereitgestellt um diese zu instanziieren, die auch in der OnCreate() methode aufgerufen wird

--- app/src/main/java/com/example/uniorganizer/MainActivity.java
+++ app/src/main/java/com/example/uniorganizer/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.uniorganizer;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.uniorganizer.Stundenplan.TimetableDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private  static  final String DATABASE_NAME = "Stundenplan";
+    private TimetableDatabase timetableDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTimetableDatabase();
     }
+
+    private void initTimetableDatabase(){
+        timetableDatabase = Room.databaseBuilder(getApplicationContext(),TimetableDatabase.class, DATABASE_NAME).build();
+
+    }
+
+
 }
