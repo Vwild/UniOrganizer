7439ee264db7c4ffb22ed23b1e6549ff0aae2541
DyNaCored <46749738+DyNaCored@users.noreply.github.com>
2019-09-07 10:20:39 +0200

    Short update for Vincent

--- app/src/main/java/com/example/uniorganizer/Stundenplan/MondayActivity.java
+++ app/src/main/java/com/example/uniorganizer/Stundenplan/MondayActivity.java
@@ -2,6 +2,10 @@ package com.example.uniorganizer.Stundenplan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.uniorganizer.R;
 
@@ -10,5 +14,20 @@ public class MondayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monday);
+
+        TextView textViewDay;
+        TextView textViewHintAddLecture;
+        TextView textViewHintName;
+        TextView textViewHintRoom;
+        TextView textViewHintBeginning;
+        TextView textViewHintEnd;
+        EditText editTextLectureName;
+        EditText editTextRoomNumber;
+        EditText editTextStartTime;
+        EditText editTextEndTime;
+        Button buttonAddLecture;
+        Button buttonBack;
+        Button buttonAddDay;
+        ListView listViewDay;
     }
 }
