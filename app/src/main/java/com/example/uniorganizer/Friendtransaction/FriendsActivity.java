503c81594a909f8e3c2e58ac49d7f70e1588d8fb
Valentin Wild <Valentin.Wild@stud.uni-regensburg.de>
2019-09-10 20:43:02 +0200

    UserItemAdpter und Useritems hinzugefügt, FriendActivity bearbeitet

--- app/src/main/java/com/example/uniorganizer/Friendtransaction/FriendsActivity.java
+++ app/src/main/java/com/example/uniorganizer/Friendtransaction/FriendsActivity.java
@@ -6,35 +6,64 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.uniorganizer.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
+
 public class FriendsActivity extends AppCompatActivity {
-    Button buttonBack;
+
+    Button buttonBack ,buttonSearch;
+    EditText editTextSearch;
+    ListView listViewUsers;
+
+    private ArrayList<User> users ;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null){
-            Toast.makeText(FriendsActivity.this,"logged in",Toast.LENGTH_SHORT).show();
-        }
-        if (user == null){
-            Intent intent = new Intent(FriendsActivity.this, ActivityLogin.class);
-            startActivity(intent);
-        }
-        buttonBack = (Button) findViewById(R.id.button_back);
+        findViews();
+        setOnClicklistener();
+        checkLogin();
+    }
+
+    private void findViews() {
+        buttonBack = findViewById(R.id.button_back);
+        buttonSearch = findViewById(R.id.button_search_friend);
+        editTextSearch = findViewById(R.id.editText_search_friend);
+        listViewUsers = findViewById(R.id.listView_users);
+    }
+
+    private void setOnClicklistener() {
+
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 finish();
-
             }
         });
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do ...
+            }
+        });
+    }
+
+    private void checkLogin(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(FriendsActivity.this, ActivityLogin.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/uniorganizer/Friendtransaction/UserItemAdapter.java
@@ -0,0 +1,49 @@
+package com.example.uniorganizer.Friendtransaction;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.uniorganizer.R;
+
+import java.util.ArrayList;
+
+
+public class UserItemAdapter extends ArrayAdapter<User> {
+
+    private ArrayList<User> users;
+    private Context context;
+
+    public UserItemAdapter(Context context, ArrayList<User> users ) {
+        super(context, R.layout.user_entry_item , users);
+        this.users = users ;
+        this.context = context;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.timetable_entry_item, null);
+        }
+
+        TextView username = v.findViewById(R.id.textView_username_entry_item);
+        ImageView symbol = v.findViewById(R.id.symbolPresent);
+
+        User user = users.get(position);
+        username.setText(user.getUsername());
+        if(user.isPresentAtUni()){
+            symbol.setImageResource(R.drawable.ic_school_green_24dp);
+        }else {
+            symbol.setImageResource(R.drawable.ic_school_red_24dp);
+        }
+
+        return v;
+    }
+}
